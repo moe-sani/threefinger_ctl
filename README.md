82b585101073733ef1ddd98bb444c182c85dba32
Moe Sani <moefattahi.sani@dyson.com>
2023-07-06 21:00:16 +0100

    ros2 messages working now + added some readme

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # threefinger_ctl
 ros2 package for three finger tool v2
+
+# Environment
+
+deactivate your conda environment if you have any
+
+```
+conda deactivate
+```
+
+This package is built based on ROS2 [Foxy](https://docs.ros.org/en/foxy/Tutorials/Beginner-CLI-Tools/Configuring-ROS2-Environment.html)
+
+Source the environment
+```
+source install/setup.bash
+```
+
+From the root of your workspace (ros2_ws), you can now build your packages using the command:
+```
+colcon build
+```
+
+To build only the my_package package next time, you can run:
+
+```console
+colcon build --packages-select my_package
+```
+
+
+then run the driver package to connect to EPOS driver
+
+```
+ros2 run threefinger_ctl epos_node
+```
+
+then run the publisher node:
+
+```
+ros2 run py_pubsub talker
+```
+
+To see the data being published on a topic, use:
+
+```
+ros2 topic echo <topic_name>
+```
--- src/epos_node.cpp
+++ src/epos_node.cpp
@@ -8,22 +8,22 @@
 #include "example_interfaces/msg/float64_multi_array.hpp"
 
 using namespace std::chrono_literals;
+// void *deviceHandle;											// Create handle to EPOS device
+threefinger_ctl::eposdriver ePos;							// Create Motor object
+void *deviceHandle = ePos.deviceOpen("USB0", 1);					// Open USB communication	
 
 // A ros2 node that subscribes to the joint angles published by the controller node and utilises epos driver to control the fingers
 class EposNode : public rclcpp::Node
 {
   public:
-    threefinger_ctl::eposdriver ePos;							// Create Motor object
-    void *deviceHandle;											// Create handle to EPOS device
     
     EposNode() : Node("epos_node")
     {
       // Initialise EPOS
-	    deviceHandle = ePos.deviceOpen("USB0", 1);					// Open USB communication	
       init_epos();													// Initialise EPOS
       
       subscription_ = this->create_subscription<example_interfaces::msg::Float64MultiArray>(
-      "joint_angles", 10, std::bind(&EposNode::topic_callback, this, std::placeholders::_1));
+      "/joint_angles", 10, std::bind(&EposNode::topic_callback, this, std::placeholders::_1));
     }
 
     ~EposNode()
@@ -48,6 +48,10 @@ class EposNode : public rclcpp::Node
       ePos.nodeEnable(deviceHandle, 1);
       ePos.nodeEnable(deviceHandle, 2);
       ePos.nodeEnable(deviceHandle, 3);
+
+      ePos.nodeMotion(deviceHandle, 1, 10);				
+      ePos.nodeMotion(deviceHandle, 2, 20); 						
+      ePos.nodeMotion(deviceHandle, 3, 30); 			
     }
 
     void end_epos()
@@ -67,8 +71,6 @@ class EposNode : public rclcpp::Node
       // Close communication
       ePos.deviceClose(deviceHandle, "USB0", 1);	// Close USB communication
     }
-
-  private:
     void moveFinger(int finger, double theta)
     {
       // move the finger to the specified angle
@@ -81,6 +83,8 @@ class EposNode : public rclcpp::Node
       ePos.nodeMotion(deviceHandle, 2, theta2);
       ePos.nodeMotion(deviceHandle, 3, theta3);
     }
+  private:
+
     void topic_callback(const example_interfaces::msg::Float64MultiArray::SharedPtr msg) const
     {
       // print all the joint angles
@@ -89,6 +93,10 @@ class EposNode : public rclcpp::Node
       // moveFinger(1, msg->data[0]);
       // moveFinger(2, msg->data[1]);
       // moveFinger(3, msg->data[2]);
+      // moveAllFingers(msg->data[0], msg->data[1], msg->data[2]);
+      ePos.nodeMotion(deviceHandle, 1, msg->data[0]);
+      ePos.nodeMotion(deviceHandle, 2, msg->data[1]);
+      ePos.nodeMotion(deviceHandle, 3, msg->data[2]);
 
 
     }
